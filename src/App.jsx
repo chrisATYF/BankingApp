c9680adf4b265ce08c12531e712bec9dd0fe6f56
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-07-18 21:28:57 -0500

    Working on Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import styles from './style'
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components'
+import styles from './style';
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from './components';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">

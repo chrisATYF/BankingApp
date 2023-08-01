4874a67968fe14f8422f6dd5a6ba238ae53457c5
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-07-31 22:06:09 -0500

    Icons added

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,12 +2,25 @@ import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from './Button';
 
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? "mb-6" : "mb-0"} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+  </div>
+)
+
 const Business = () => (
   <section id="features" className={layout.section}>
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /> we'll handle the money.</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards, and saving money.</p>
       <Button styles="mt-10" />
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
     </div>
   </section>
 )
